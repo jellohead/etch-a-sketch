54ae390e21017b6fbc67d5b387ee2c14cfe6c2bf
Pat Escalona <pat.escalona7@gmail.com>
2022-09-18 22:30:30 -0500

    Added 16 grid elements
    Need to get it to 16x16 but still working out
    how to do that

--- eas.js
+++ eas.js
@@ -1,1 +1,18 @@
 // eas.js
+// container div id = grid-container
+const body = document.body;
+// body.append("hello world");
+
+
+console.log("Hello world");
+let text;
+let newDiv;
+const wrapper = document.getElementById('grid-container');
+
+for (let i = 0; i < 16; i++) {
+    newDiv = document.createElement("div");
+    text = document.createTextNode(`new div ${i + 1}`);
+    newDiv.appendChild(text);
+    newDiv.classList.add("grid-item");
+    wrapper.appendChild(newDiv);
+}
