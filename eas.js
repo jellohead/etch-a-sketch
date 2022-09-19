fa82e51710a9f0157f41633a1ad816d7f10bf6ed
Pat Escalona <pat.escalona7@gmail.com>
2022-09-19 14:07:18 -0500

    Mouseover changes div text color now
    next step is to have mouseover change div
    color instead of just the text.

--- eas.js
+++ eas.js
@@ -8,6 +8,8 @@ console.log("Hello world");
 let text;
 let newDiv;
 const wrapper = document.getElementById('grid-container');
+let gItems;
+
 
 for (let i = 0; i < 256; i++) {
     newDiv = document.createElement("div");
@@ -15,4 +17,17 @@ for (let i = 0; i < 256; i++) {
     newDiv.appendChild(text);
     newDiv.setAttribute("id", "grid-item");
     wrapper.appendChild(newDiv);
+    newDiv.addEventListener('mouseover', e => {
+        e.target.style.color = 'red';
+    });
 }
+
+// gItems = document.querySelectorAll('grid-item');
+// console.log("gItems = " + gItems);
+// gItems.forEach(addEventListener('mouseover', e => {
+    // console.log(e);
+    // e.target.style.color = 'red';
+
+// }));
+
+
